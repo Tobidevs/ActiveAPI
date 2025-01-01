e3d6f15250df65788df6f09ad1240b01898889ec
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-01-01 16:39:49 -0600

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        customBlack: '#0c0c0e', // Custom black color
+        customDarkPurple: '#0f081b', // Custom dark purple color
+        customPurple: '#8c52ff', // Custom purple color
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'], // Add the Poppins font
+      },
+    },
+  },
+  plugins: [],
+}
+
